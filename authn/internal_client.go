bcbe40cbbb77da1111c362d6dc7a1762b49b4746
Lance Ivy <lance@cainlevy.net>
2019-09-10 19:36:13 -0700

    streamline private api request construction

--- authn/internal_client.go
+++ authn/internal_client.go
@@ -3,6 +3,7 @@ package authn
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -19,6 +20,14 @@ type internalClient struct {
 	username string
 	password string
 }
+
+const (
+	delete = "DELETE"
+	get    = "GET"
+	patch  = "PATCH"
+	post   = "POST"
+	put    = "PUT"
+)
 
 func newInternalClient(base, username, password string) (*internalClient, error) {
 	// ensure that base ends with a '/', so ResolveReference() will work as desired
@@ -68,24 +77,12 @@ func (ic *internalClient) Key(kid string) ([]jose.JSONWebKey, error) {
 
 //GetAccount gets the account details for the specified account id
 func (ic *internalClient) GetAccount(id string) (*Account, error) {
-	//Setup the request
-	req, err := http.NewRequest("GET", ic.absoluteURL("accounts/"+id), nil)
-	if err != nil {
-		return nil, err
-	}
-	req.SetBasicAuth(ic.username, ic.password)
-
-	//Do the request
-	resp, err := ic.client.Do(req)
+	resp, err := ic.doWithAuth(get, "accounts/"+id, nil)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		return nil, fmt.Errorf("received %d from %s", resp.StatusCode, ic.absoluteURL("accounts/"+id))
-	}
 
-	//Marshal the result
 	data := struct {
 		Result Account `json:"result"`
 	}{}
@@ -100,166 +97,56 @@ func (ic *internalClient) GetAccount(id string) (*Account, error) {
 
 //Update updates the account with the specified id
 func (ic *internalClient) Update(id, username string) error {
-	//Setup request body
 	form := url.Values{}
 	form.Add("username", username)
 
-	//Setup the request
-	req, err := http.NewRequest("PATCH", ic.absoluteURL("accounts/"+id), strings.NewReader(form.Encode()))
-	if err != nil {
-		return err
-	}
-	req.SetBasicAuth(ic.username, ic.password)
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	//Do the request
-	resp, err := ic.client.Do(req)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("received %d from %s", resp.StatusCode, ic.absoluteURL("accounts/"+id))
-	}
-
-	return nil
+	_, err := ic.doWithAuth(patch, "accounts/"+id, strings.NewReader(form.Encode()))
+	return err
 }
 
 //LockAccount locks the account with the specified id
 func (ic *internalClient) LockAccount(id string) error {
-	//Setup the request
-	req, err := http.NewRequest("PATCH", ic.absoluteURL("accounts/"+id+"/lock"), nil)
-	if err != nil {
-		return err
-	}
-	req.SetBasicAuth(ic.username, ic.password)
-
-	//Do the request
-	resp, err := ic.client.Do(req)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("received %d from %s", resp.StatusCode, ic.absoluteURL("accounts/"+id+"/lock"))
-	}
-
-	return nil
+	_, err := ic.doWithAuth(patch, "accounts/"+id+"/lock", nil)
+	return err
 }
 
 //UnlockAccount unlocks the account with the specified id
 func (ic *internalClient) UnlockAccount(id string) error {
-	//Setup the request
-	req, err := http.NewRequest("PATCH", ic.absoluteURL("accounts/"+id+"/unlock"), nil)
-	if err != nil {
-		return err
-	}
-	req.SetBasicAuth(ic.username, ic.password)
-
-	//Do the request
-	resp, err := ic.client.Do(req)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("received %d from %s", resp.StatusCode, ic.absoluteURL("accounts/"+id+"/unlock"))
-	}
-
-	return nil
+	_, err := ic.doWithAuth(patch, "accounts/"+id+"/unlock", nil)
+	return err
 }
 
 //ArchiveAccount archives the account with the specified id
 func (ic *internalClient) ArchiveAccount(id string) error {
-	//Setup the request
-	req, err := http.NewRequest("DELETE", ic.absoluteURL("accounts/"+id), nil)
-	if err != nil {
-		return err
-	}
-	req.SetBasicAuth(ic.username, ic.password)
-
-	//Do the request
-	resp, err := ic.client.Do(req)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("received %d from %s", resp.StatusCode, ic.absoluteURL("accounts/"+id))
-	}
-
-	return nil
+	_, err := ic.doWithAuth(delete, "accounts/"+id, nil)
+	return err
 }
 
 //ImportAccount imports an existing account
 func (ic *internalClient) ImportAccount(username, password string, locked bool) error {
-	//Setup request body
 	form := url.Values{}
 	form.Add("username", username)
 	form.Add("password", password)
 	form.Add("locked", strconv.FormatBool(locked))
 
-	//Setup the request
-	req, err := http.NewRequest("POST", ic.absoluteURL("accounts/import"), strings.NewReader(form.Encode()))
-	if err != nil {
-		return err
-	}
-	req.SetBasicAuth(ic.username, ic.password)
-	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-
-	//Do the request
-	resp, err := ic.client.Do(req)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != 201 {
-		return fmt.Errorf("received %d from %s", resp.StatusCode, ic.absoluteURL("accounts/import"))
-	}
-
-	return nil
+	_, err := ic.doWithAuth(post, "accounts/import", strings.NewReader(form.Encode()))
+	return err
 }
 
 //ExpirePassword expires the users current sessions and flags the account for a required password change on next login
 func (ic *internalClient) ExpirePassword(id string) error {
-	//Setup the request
-	req, err := http.NewRequest("PATCH", ic.absoluteURL("accounts/"+id+"/expire_password"), nil)
-	if err != nil {
-		return err
-	}
-	req.SetBasicAuth(ic.username, ic.password)
-
-	//Do the request
-	resp, err := ic.client.Do(req)
-	if err != nil {
-		return err
-	}
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("received %d from %s", resp.StatusCode, ic.absoluteURL("accounts/"+id+"/expire_password"))
-	}
-
-	return nil
+	_, err := ic.doWithAuth(patch, "accounts/"+id+"/expire_password", nil)
+	return err
 }
 
 //ServiceStats returns the raw request from the /stats endpoint
 func (ic *internalClient) ServiceStats() (*http.Response, error) {
-	//Setup the request
-	req, err := http.NewRequest("GET", ic.absoluteURL("stats"), nil)
-	if err != nil {
-		return nil, err
-	}
-	req.SetBasicAuth(ic.username, ic.password)
-
-	//Do the request
-	return ic.client.Do(req)
+	return ic.doWithAuth(get, "stats", nil)
 }
 
 //ServerStats returns the raw request from the /metrics endpoint
 func (ic *internalClient) ServerStats() (*http.Response, error) {
-	//Setup the request
-	req, err := http.NewRequest("GET", ic.absoluteURL("metrics"), nil)
-	if err != nil {
-		return nil, err
-	}
-	req.SetBasicAuth(ic.username, ic.password)
-
-	//Do the request
-	return ic.client.Do(req)
+	return ic.doWithAuth(get, "metrics", nil)
 }
 
 func (ic *internalClient) absoluteURL(path string) string {
@@ -284,6 +171,27 @@ func (ic *internalClient) get(path string, dest interface{}) (int, error) {
 		return resp.StatusCode, err
 	}
 	return resp.StatusCode, nil
+}
+
+func (ic *internalClient) doWithAuth(verb string, path string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(verb, ic.absoluteURL(path), body)
+	if err != nil {
+		return nil, err
+	}
+	req.SetBasicAuth(ic.username, ic.password)
+
+	if verb == post || verb == patch || verb == put {
+		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	}
+
+	resp, err := ic.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if !isStatusSuccess(resp.StatusCode) {
+		return nil, fmt.Errorf("received %d from %s", resp.StatusCode, ic.absoluteURL(path))
+	}
+	return resp, nil
 }
 
 func isStatusSuccess(statusCode int) bool {
