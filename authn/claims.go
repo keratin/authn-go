8816dcd0dff4b85d82da1b28a41bdbac023258ab
Alex Ullrich <aullrich@candy.com>
2023-04-18 14:28:17 -0400

    claims: include session ID / SID
    Marshaled to 'sid' JWT claim https://www.iana.org/assignments/jwt/jwt.xhtml#claims

--- authn/claims.go
+++ authn/claims.go
@@ -3,6 +3,7 @@ package authn
 import "gopkg.in/square/go-jose.v2/jwt"
 
 type Claims struct {
-	AuthTime *jwt.NumericDate `json:"auth_time"`
+	AuthTime  *jwt.NumericDate `json:"auth_time"`
+	SessionID string           `json:"sid"`
 	jwt.Claims
 }
